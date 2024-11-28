47c52f535fbde5f8d868e4ed822ed3c538308b60
Gugle <gugle@st.xatu.edu.cn>
2024-11-28 15:15:42 +0800

    设置主类

---
+++ src/main/java/dev/dubhe/p2p/P2PConnector.java
@@ -0,0 +1,10 @@
+package dev.dubhe.p2p;
+
+public class P2PConnector {
+    public static void main(String[] args) {
+        try (Console console = new Console()) {
+            System.out.println(111);
+            while (console.isStop()) console.tick();
+        }
+    }
+}
